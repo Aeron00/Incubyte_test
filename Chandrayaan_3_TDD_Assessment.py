309570e3a26ec293ea6874e8a5e55a9c7e8c5edd
aeron00 <aeronmahida00@gmail.com>
2023-08-03 22:34:03 +0530

    second commit added unit test file and updated both files

---
+++ Chandrayaan_3_TDD_Assessment.py
@@ -0,0 +1,20 @@
+"""
+inputs :
+starting position (x,y,z)
+init direction anyone(N,S,E,W,U,P)
+commands 
+
+outputs :
+final position and 
+final direction
+"""
+
+def control_lunar_space_craft(start_pos : tuple, init_direc : str, commands_lst : list):
+    final_pos = (1,1,1)
+    final_dir = "S"
+    return final_pos,   final_dir
+
+
+
+
+
