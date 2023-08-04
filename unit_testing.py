8a9433b57301556eaf2f441184fea8462b32a536
aeron00 <aeronmahida00@gmail.com>
2023-08-04 13:56:18 +0530

    updated test file with given example to test

--- unit_testing.py
+++ unit_testing.py
@@ -4,6 +4,7 @@ from Chandrayaan_3_TDD_Assessment import control_lunar_space_craft
 class TestCase(unittest.TestCase):
     def test_final_pos_and_direction(self):
         result = control_lunar_space_craft((0,0,0),"N",['f','r','u','b','l'])
-        self.assertEqual(result,((1,1,1),"S"))
+        self.assertEqual(result,((0,1,-1),"W"))
 
-unittest.main()
+if __name__ == "__main__":
+    unittest.main()
