8a3c4b7f2fa9fe5483c83276718f8ae9ca91816e
aeron00 <aeronmahida00@gmail.com>
2023-08-04 15:31:00 +0530

    added second test

--- unit_testing.py
+++ unit_testing.py
@@ -2,9 +2,13 @@ import unittest
 from Chandrayaan_3_TDD_Assessment import control_lunar_space_craft
 
 class TestCase(unittest.TestCase):
-    def test_final_pos_and_direction(self):
+    def test1_final_pos_and_direction(self):
         result = control_lunar_space_craft((0,0,0),"N",['f','r','u','b','l'])
         self.assertEqual(result,((0,1,-1),"W"))
+
+    def test2_final_pos_and_direction(self):
+        result = control_lunar_space_craft((1,1,1),"S",['r','f','u','l','b','r'])
+        self.assertEqual(result,((1,1,1),"N"))
 
 if __name__ == "__main__":
     unittest.main()
